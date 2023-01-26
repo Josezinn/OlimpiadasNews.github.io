5d7cc0e4e24836efe31c66042ee5aa12715ea0a3
José Francisco <franciscojosefrancisco341@gmail.com>
2023-01-26 11:26:23 -0300

    atualização na aula do dia 26 de jan
    Introdução js, mudança de tema.

---
+++ script.js
@@ -0,0 +1,7 @@
+//let entrada = parseInt(prompt("Digite o ano que você nasceu"))
+//alert(2023+entrada)
+//alert(typeof entrada)
+
+let section=document.querySelector("section")
+ section.classList.add("dark")
+ section.classList.remove("light")
