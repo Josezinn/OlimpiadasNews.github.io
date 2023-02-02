ead45ee17d5995cb6ddd21ea15d04c9b7063669a
José Francisco <franciscojosefrancisco341@gmail.com>
2023-02-02 00:16:31 -0300

    Aprendendo e Implementando switch toggle

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 //alert(2023+entrada)
 //alert(typeof entrada)
 
-let section=document.querySelector("section")
- section.classList.add("dark")
- section.classList.remove("light")
+//let section=document.querySelector("section")
+//section.classList.add("dark")
+//section.classList.remove("light")
+
+//function toJedSide(){
+//   section.classList.add("light-theme")
+//   section.classList.remove("dark-theme")
+//}
+
+/*const switchToggle = document.getElementById("switchToggle");
+
+ switchToggle.addEventListener("change", function() {
+   if (this.checked) {
+     document.body.classList.add("dark-mode");
+   } else {
+     document.body.classList.remove("dark-mode");
+   }
+ });*/
+
+const switchToggle = document.querySelector("#switchToggle");
+
+switchToggle.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+  
+});
