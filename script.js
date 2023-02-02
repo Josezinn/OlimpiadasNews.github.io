4b07176077ff484551b5184f0c39caaa07293714
José Francisco <franciscojosefrancisco341@gmail.com>
2023-02-02 11:03:29 -0300

    Ajustes no Toggle

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-//let entrada = parseInt(prompt("Digite o ano que você nasceu"))
-//alert(2023+entrada)
-//alert(typeof entrada)
-
-//let section=document.querySelector("section")
-//section.classList.add("dark")
-//section.classList.remove("light")
-
-//function toJedSide(){
-//   section.classList.add("light-theme")
-//   section.classList.remove("dark-theme")
-//}
-
-/*const switchToggle = document.getElementById("switchToggle");
-
- switchToggle.addEventListener("change", function() {
-   if (this.checked) {
-     document.body.classList.add("dark-mode");
-   } else {
-     document.body.classList.remove("dark-mode");
-   }
- });*/
-
 const switchToggle = document.querySelector("#switchToggle");
 
 switchToggle.addEventListener("change", function () {
@@ -29,5 +6,4 @@ switchToggle.addEventListener("change", function () {
   } else {
     document.body.classList.remove("dark-mode");
   }
-  
 });
